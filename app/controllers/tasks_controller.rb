cdcf39b9c8b27b2a3ca0b7ce11211e35f2ca807f
tsubasa namatame <nyshk97@gmail.com>
2019-11-19 20:41:46 +0900

    add destroy func(complete)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -19,7 +19,13 @@ class TasksController < ApplicationController
   def update
     @task = Task.find(params[:id])
     @task.update(task_params)
-    redirect_to tasks_path
+    redirect_to tasks_url
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_url
   end
 
   private
