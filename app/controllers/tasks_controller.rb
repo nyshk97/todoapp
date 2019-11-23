f69b2ca98246c912447c65fdbe6964810a897b98
tsubasa namatame <nyshk97@gmail.com>
2019-11-23 18:37:29 +0900

    Modify path to url

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,7 +10,7 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     if @task.save
-      redirect_to tasks_path
+      redirect_to tasks_url
     else
       render 'tasks/new'
     end
